0e8865661754def0bc2f5f9687022b4d63d7af1b
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-05-01 17:34:03 -0400

    [Auth] Change responses in auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -26,16 +26,16 @@ export const signUp = async (req: Request, res: Response) => {
 export const signIn = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Email or password is wrong' });
+    if (!user) return res.status(400).json({ error: 400, message: 'Email or password is wrong' });
 
     const correctPass = await user.validatePassword(password, user.password);
-    if(!correctPass) return res.status(400).json({ message: 'Invalid password' });
+    if(!correctPass) return res.status(400).json({ error: 400, message: 'Invalid password' });
 
     const token: string = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET || 'asd1234', {
         expiresIn: 60 * 60 * 24,
     });
 
-    res.header('auth-token', token).json(user);
+    res.json({error: null, data: { token, ...user }});
 };
 
 export const profile = async (req: Request, res: Response) => {
